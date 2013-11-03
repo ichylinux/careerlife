e44a8eb8700ba86e41bac5f4643705075493163a
ichy <ichylinux@gmail.com>
2013-11-03 12:35:49 +0900

    p7

--- app/models/career_detail.rb
+++ app/models/career_detail.rb
@@ -1,2 +1,5 @@
+# coding: UTF-8
+
 class CareerDetail < ActiveRecord::Base
+  validates :project_name, :presence => true
 end
--- db/migrate/20130307031034_create_careers.rb
+++ db/migrate/20130307031034_create_careers.rb
@@ -1,8 +1,7 @@
 class CreateCareers < ActiveRecord::Migration
   def change
     create_table :careers do |t|
-      t.string :last_name
-      t.string :first_name
+      t.string :name
       t.date :birthday
       t.string :gender
 
