1e283a2484f3928ce49cc91b885b195c6e437658
ichy <ichylinux@gmail.com>
2014-05-12 21:22:23 +0900

    p4

--- features/step_definitions/開発日記/2013-03-07.rb
+++ features/step_definitions/開発日記/2013-03-07.rb
@@ -1,7 +1,9 @@
 # coding: UTF-8
 
 前提 /^rails g migration add_column_introduction_on_careers$/ do
-  show 'db/migrate/20130308164115_add_column_introduction_on_careers.rb', :as => ['auto', 'edit']
+  diff 'db/migrate/20130308164115_add_column_introduction_on_careers.rb',
+      File.join(File.dirname(__FILE__), '20130308164115_add_column_introduction_on_careers.rb'),
+      :as => ['auto', 'edit']
 end
 
 前提 /^rake db:migrate$/ do
---
+++ features/step_definitions/開発日記/20130308164115_add_column_introduction_on_careers.rb
@@ -0,0 +1,7 @@
+class AddColumnIntroductionOnCareers < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
