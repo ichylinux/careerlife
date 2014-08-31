8af8b26e396bd97eeb71484e19a0149169b6af00
ichy <ichylinux@gmail.com>
2014-08-31 19:14:19 +0900

    p5

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Careerlife::Application.config.secret_token = 'ac4227bae3809def9fda2a736818beb2dc09af6432a490a1ccb28381a5e1c2bfd8cf7b147218f9d87d6cb2f39ffe404356444971fefca33e5f402bc570f0275e'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Careerlife::Application.config.secret_key_base = 'a00aa171f248fea41fd3b5bdb4b037cce10601c49bd81140f88ddaf9e2541f2ae4b42b63cadd339f99134f74463a60f2c7019a026bdaa725298d5e9bdfb447b1'
